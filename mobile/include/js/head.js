82cb1d239c265aaf870eda8f0e130a3a1c18946c
Muhyeon <muhyeon04@gmail.com>
2025-09-18 17:49:58 +0900

    aaaa

--- mobile/include/js/head.js
+++ mobile/include/js/head.js
@@ -25,6 +25,7 @@ str += '<link rel="stylesheet" href="../mobile/include/css/contents_f.css">';
 str += '<link rel="stylesheet" href="../mobile/include/css/contents_g.css">';
 str += '<link rel="stylesheet" href="../mobile/include/css/contents_h.css">';
 str += '<link rel="stylesheet" href="../mobile/include/css/layout_scss.css">';
+str += '<link rel="stylesheet" href="../mobile/include/css/temp.css">';
 str += '<script src="../mobile/include/js/jquery-3.7.1.min.js"></script>';
 str += '<script src="../mobile/js/contents.js"></script>';
         
